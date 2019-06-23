f48f04c05b578f6a2136b62090c0e8cfc5666da0
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2019-06-23 20:02:09 +0200

    build

--- build/precache-manifest.b7af87fde3c45eecac8cbfebe7d75428.js
+++ build/precache-manifest.b7af87fde3c45eecac8cbfebe7d75428.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b9717d025a8378e032572492ebd78526",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "1b9640bfe3859b07e4b8",
-    "url": "/static/css/main.95cb6a6c.chunk.css"
+    "url": "./static/css/main.95cb6a6c.chunk.css"
   },
   {
     "revision": "2967036c8e6086717a74",
-    "url": "/static/js/2.bbc34431.chunk.js"
+    "url": "./static/js/2.bbc34431.chunk.js"
   },
   {
     "revision": "1b9640bfe3859b07e4b8",
-    "url": "/static/js/main.419204fa.chunk.js"
+    "url": "./static/js/main.419204fa.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "./static/js/runtime~main.a8a9905a.js"
   }
 ]);
